0339003ef1aa3a7d42da60c6013eab5ca17459b7
trungnotchung <trung09072004@gmail.com>
2025-05-18 18:59:54 +0700

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,25 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 interface Config {
   port: number;
   nodeEnv: string;
+  infura: {
+    projectId: string;
+    projectSecret: string;
+  };
 }
 
 const config: Config = {
   port: Number(process.env.PORT) || 3000,
-  nodeEnv: process.env.NODE_ENV || 'development',
+  nodeEnv: process.env.NODE_ENV || "development",
+  infura: {
+    projectId: process.env.INFURA_IPFS_PROJECT_ID || "",
+    projectSecret: process.env.INFURA_IPFS_PROJECT_SECRET || "",
+  },
 };
+
+console.log(config);
 
 export default config;
